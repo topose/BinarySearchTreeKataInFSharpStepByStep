717631e60ecd68fdb04e6ed8f750a858cb0fceba
topose <david.carballo.gm@gmail.com>
2015-07-24 10:09:58 +0200

    Refactor: simplified type specification for Assert.Equal with lists

--- BinSearchTreeKata/BinSearchTreeKata/BinarySearchTreeSpec.fs
+++ BinSearchTreeKata/BinSearchTreeKata/BinarySearchTreeSpec.fs
@@ -10,20 +10,20 @@ open Xunit.Abstractions
 let ``Create a tree with just a root`` () =
     let sut = EmptyTree.FromList([4])
 
-    Assert.Equal<Collections.Generic.IEnumerable<int>>([4], sut.InOrder)
-    Assert.Equal<Collections.Generic.IEnumerable<int>>([4], sut.PreOrder)
+    Assert.Equal<int list>([4], sut.InOrder)
+    Assert.Equal<int list>([4], sut.PreOrder)
 
 
 [<Fact>]
 let ``Create a tree with root, left and right son`` () =
     let sut = EmptyTree.FromList([4; 1; 5])
 
-    Assert.Equal<Collections.Generic.IEnumerable<int>>([1; 4; 5], sut.InOrder)
-    Assert.Equal<Collections.Generic.IEnumerable<int>>([4; 1; 5], sut.PreOrder)
+    Assert.Equal<int list>([1; 4; 5], sut.InOrder)
+    Assert.Equal<int list>([4; 1; 5], sut.PreOrder)
 
 [<Fact>]
 let ``Create a complex tree`` () =
     let sut = EmptyTree.FromList([4; 2; 6; 3; 1; 5; 7])
 
-    Assert.Equal<Collections.Generic.IEnumerable<int>>([1; 2; 3; 4; 5; 6; 7], sut.InOrder)
-    Assert.Equal<Collections.Generic.IEnumerable<int>>([4; 2; 1; 3; 6; 5; 7], sut.PreOrder)
+    Assert.Equal<int list>([1; 2; 3; 4; 5; 6; 7], sut.InOrder)
+    Assert.Equal<int list>([4; 2; 1; 3; 6; 5; 7], sut.PreOrder)
