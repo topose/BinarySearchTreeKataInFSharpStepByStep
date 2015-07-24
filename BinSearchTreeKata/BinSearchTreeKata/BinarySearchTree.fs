d64059b3679542ccdf54686a746ca20f6bddd8a8
topose <david.carballo.gm@gmail.com>
2015-07-24 09:11:02 +0200

    It creates tress with just the root value

---
+++ BinSearchTreeKata/BinSearchTreeKata/BinarySearchTree.fs
@@ -0,0 +1,14 @@
+﻿namespace BinarySearchTreeKata
+
+type BinarySearchTree = 
+    | EmptyTree
+    | BinaryTree of int * BinarySearchTree * BinarySearchTree
+
+    member this.Insert(value:int) = 
+        BinaryTree(value, EmptyTree, EmptyTree)
+
+    member this.InOrder =
+        match this with
+        | EmptyTree -> []
+        | BinaryTree (value, EmptyTree, EmptyTree) -> [value]
+        | BinaryTree (value, left, right) -> []
---
+++ BinSearchTreeKata/BinSearchTreeKata/BinarySearchTreeSpec.fs
@@ -0,0 +1,13 @@
+﻿module BinarySearchTreeSpec
+
+open System
+open BinarySearchTreeKata
+
+open Xunit
+open Xunit.Abstractions
+
+[<Fact>]
+let ``Create a tree with just a root`` () =
+    let sut = EmptyTree.Insert(4)
+
+    Assert.Equal<Collections.Generic.IEnumerable<int>>([4], sut.InOrder)
