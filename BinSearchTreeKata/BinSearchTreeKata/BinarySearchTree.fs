d38ce5fce68be8fae12a3af3b090afadd03485a9
topose <david.carballo.gm@gmail.com>
2015-07-24 09:28:04 +0200

    It creates a tree with root, left and right son

--- BinSearchTreeKata/BinSearchTreeKata/BinarySearchTree.fs
+++ BinSearchTreeKata/BinSearchTreeKata/BinarySearchTree.fs
@@ -7,12 +7,13 @@ type BinarySearchTree =
     member this.Insert(value:int) = 
         match this with
         | EmptyTree -> BinaryTree(value, EmptyTree, EmptyTree)
-        | BinaryTree (root, left, right) -> 
+        | BinaryTree (root, left, right) as node -> 
             if (value < root) then
-                BinaryTree(root, EmptyTree.Insert(value), EmptyTree)
+                BinaryTree(root, EmptyTree.Insert(value), right)
+            elif (value > root) then
+                BinaryTree(root, left, EmptyTree.Insert(value))
             else
-                BinaryTree(root, EmptyTree, EmptyTree.Insert(value))
-        
+                node
 
     member this.InOrder =
         match this with
@@ -22,4 +23,6 @@ type BinarySearchTree =
             [left_value] @ [value]
         | BinaryTree (value, EmptyTree, BinaryTree(right_value, EmptyTree, EmptyTree)) -> 
             [value] @ [right_value] 
+        | BinaryTree (value, BinaryTree(left_value, EmptyTree, EmptyTree), BinaryTree(right_value, EmptyTree, EmptyTree)) -> 
+            [left_value] @ [value] @ [right_value] 
         | BinaryTree (value, left, right) -> []
--- BinSearchTreeKata/BinSearchTreeKata/BinarySearchTreeSpec.fs
+++ BinSearchTreeKata/BinSearchTreeKata/BinarySearchTreeSpec.fs
@@ -23,3 +23,9 @@ let ``Create a tree with just root and right son`` () =
     let sut = EmptyTree.Insert(4).Insert(5)
 
     Assert.Equal<Collections.Generic.IEnumerable<int>>([4; 5], sut.InOrder)
+
+[<Fact>]
+let ``Create a tree with root, left and right son`` () =
+    let sut = EmptyTree.Insert(4).Insert(1).Insert(5)
+
+    Assert.Equal<Collections.Generic.IEnumerable<int>>([1; 4; 5], sut.InOrder)
